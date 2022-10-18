10e5f2278a865d4a76e10145e28677d468e5b7a4
JiaXiangHuang <jhuang4836@bths.edu>
2022-10-18 19:58:20 -0400

    Able to turn strings into integers, finished toString method, fixed/improved most constructors. removed 2 pairSetters changed it to 1, set rounding to hundredths, finished runner class. Completion = 100%

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -10,41 +10,60 @@ public class LinearEquation {
     private double slopeX;
     private double slopeY;
     private double yIntercept;
-    private double xIntercept;
     private double distance;
 
     DecimalFormat dRound = new DecimalFormat("0.00");
 
-    public LinearEquation(int xcoord1, int ycoord1, int xcoord2, int ycoord2) {
-        x = xcoord1;
-        y = ycoord1;
-        x2 = xcoord2;
-        y2 = ycoord2;
-        slopeX = (x2 - x);
-        slopeY = (y2 - y);
-        yIntercept();
-        distance();
-        System.out.println("y = "+ slopeY + "/" + slopeX + "x + " + (int)yIntercept );
-        System.out.println((int) yIntercept);
-        System.out.println(dRound.format(distance));
-
-
+    public LinearEquation(String xcoord1, String ycoord1, String xcoord2, String ycoord2) {
+        x = Integer.parseInt(xcoord1);
+        y = Integer.parseInt(ycoord1);
+        x2 = Integer.parseInt(xcoord2);
+        y2 = Integer.parseInt(ycoord2);
+        slopeX = x2 - x;
+        slopeY = y2 - y;
     }
 
     private double slope() {
         slopeInt = (y2 - y) / (x2 - x);
         return slopeInt;
     }
-    private double yIntercept(){
-        yIntercept = (-(slopeY/slopeX)*x+y);
+
+    private double yIntercept() {
+        yIntercept = (-(slopeInt) * x + y);
         return yIntercept;
     }
-    private double distance(){
-        distance = (double)Math.sqrt((x2-x)*(x2-x) + (y2-y)*(y2-y));
+
+    private double distance() {
+        distance = Math.sqrt((x2 - x) * (x2 - x) + (y2 - y) * (y2 - y));
         return distance;
     }
-
-
-
+    private String slopeIntForm() {
+        if (yIntercept >= 0) {
+            return "y = " + slopeY + "/" + slopeX + "x + " + (int) yIntercept;
+        } else if (yIntercept < 0) {
+            return "y = " + slopeY + "/" + slopeX + "x - " + Math.abs((int) yIntercept);
+        }
+        return null;
+    }
+    private String pairSetter(int xvalue, int yvalue){
+        return "("+xvalue+","+yvalue+")";
+    }
+    public String coordinatePoint(double xvalue){
+        double y3 = slope() * xvalue + yIntercept();
+        return "("+xvalue+","+y3+")";
 
     }
+
+    @Override
+    public String toString() {
+        String pair =
+                "First Pair: " + pairSetter(x,y) + "\n" +
+                "Second Pair:  " + pairSetter(x2,y2) + "\n" +
+                "Slope of line: " + dRound.format(slope()) + "\n" +
+                "Y-Intercept: " + dRound.format(yIntercept()) + "\n" +
+                "Slope intercept form: " + slopeIntForm() + "\n" +
+                "Distance between points: " + dRound.format(distance());
+        return pair;
+    }
+}
+
--- src/Runner.java
+++ src/Runner.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
 public class Runner {
     public static void main(String[] args) {
-        LinearEquation new1 = new LinearEquation(-1,1,-2,-3);
+        Scanner s= new Scanner(System.in);
+
+        System.out.println("Enter the first x-y coordinate point: ");
+        String fCoord = s.nextLine();
+        String xcoord1 = fCoord.substring(1,fCoord.indexOf(","));
+        String ycoord1 = fCoord.substring(fCoord.indexOf(",")+1 ,fCoord.length()-1);
+        System.out.println("Enter the second x-y coordinate point: ");
+        String sCoord = s.nextLine();
+        String xcoord2 = sCoord.substring(1,sCoord.indexOf(","));
+        String ycoord2 = sCoord.substring(sCoord.indexOf(",")+1,sCoord.length()-1);
+
+        LinearEquation new1 = new LinearEquation(xcoord1,ycoord1,xcoord2,ycoord2);
+        System.out.println(new1.toString());
+        System.out.println("Enter an x value: ");
+        double xvalue = s.nextDouble();
+        System.out.println("Solved coordinate point is: " + new1.coordinatePoint(xvalue));
+
     }
+
 }
